357f436c24c77866798027efade987e881b38a76
pavel _durov <pdurov0@gmail.com>
2015-10-13 15:51:50 +0300

    windows universal app support

---
+++ src/windows/bundleIdentifier.js
@@ -0,0 +1,41 @@
+﻿
+
+
+//var cordova = require('cordova'),
+//    exec = require('cordova/exec');
+
+var BundleIdentifier = function () {
+    // constructor
+};
+
+//Interface...
+
+
+BundleIdentifier.prototype.get = function (options, success, error) {
+    cordova.exec(success, error, "BundleIdentifier", "get", options ? [options] : []);
+};
+
+var bundleIdentifier = new BundleIdentifier();
+
+module.exports = bundleIdentifier;
+
+
+//Implementation
+cordova.commandProxy.add("BundleIdentifier", {
+    get: function (successCallback, errorCallback, strInput) {
+        var result = {};
+        result.bundleId = undefined;
+        //console.log("ENTER - BundleIdentifier.get function");
+
+        if (Windows && Windows.ApplicationModel) {
+            var pckg = Windows.ApplicationModel.Package.current;
+            result.bundleId = pckg.id.fullName;
+
+            successCallback(result);
+        } else {
+            errorCallback(result);
+        }
+
+        //console.log("EXIT - BundleIdentifier.get function");
+    }
+});
